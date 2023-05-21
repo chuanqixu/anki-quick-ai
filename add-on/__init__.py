986be34ad7e87a25a66cc2fa321f5c16e0d9f728
Chuanqi Xu <466018122@qq.com>
2023-05-20 23:51:36 -0400

    update/hook for deck finishes and choose button

--- add-on/__init__.py
+++ add-on/__init__.py
@@ -2,16 +2,21 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib"))
 
-from aqt import mw
+from aqt import mw, gui_hooks
 from aqt.qt import QAction, qconnect
 
-from .controller import gen_words_story
+from .controller import gen_words_story, choose_running_add_on
 
 
 
 # create a new menu item, "test"
 action = QAction("AI Words Story", mw)
 # set it to call testFunction when it's clicked
+# qconnect(action.triggered, gen_words_story)
 qconnect(action.triggered, gen_words_story)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
+
+
+# hook for end of the deck
+gui_hooks.reviewer_will_end.append(choose_running_add_on)
--- add-on/anki.py
+++ add-on/anki.py
@@ -2,7 +2,7 @@ from aqt import mw
 
 
 
-def get_words(browse_cmd):
+def get_note_field_value_list(browse_cmd):
     card_id_list = mw.col.find_cards(browse_cmd)
-    words = [mw.col.get_card(card_id).note()[mw.addonManager.getConfig(__name__)["note_field"]] for card_id in card_id_list]
-    return words
+    field_value_list = [mw.col.get_card(card_id).note()[mw.addonManager.getConfig(__name__)["note_field"]] for card_id in card_id_list]
+    return field_value_list
