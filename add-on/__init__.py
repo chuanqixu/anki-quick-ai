cd53cda407a8eac0a14e6e153c139a09ff14c459
Chuanqi Xu <466018122@qq.com>
2023-05-21 13:50:56 -0400

    update/change the logic

--- add-on/__init__.py
+++ add-on/__init__.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib"))
 from aqt import mw, gui_hooks
 from aqt.qt import QAction, qconnect
 
-from .controller import gen_words_story, choose_running_add_on
+from .controller import gen_response, choose_running_add_on
 
 
 
@@ -13,7 +13,7 @@ from .controller import gen_words_story, choose_running_add_on
 action = QAction("AI Words Story", mw)
 # set it to call testFunction when it's clicked
 # qconnect(action.triggered, gen_words_story)
-qconnect(action.triggered, gen_words_story)
+qconnect(action.triggered, gen_response)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
 
